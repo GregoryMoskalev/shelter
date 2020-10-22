0197112d4ace9ba4dc291af71de4e580f00f72a9
Gregory Moskalev <onberfrest@gmail.com>
2020-10-23 00:35:49 +0300

    feat: close menu on click outside and menu item

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -1,7 +1,15 @@
 console.log('ヾ(＾∇＾)');
-const burger = document.querySelector('.navbar__checkbox');
+const burger = document.querySelector('.navbar__checkbox'),
+  petsBtns = document.querySelectorAll('.card-btn'),
+  burgerBlackout = document.querySelector('.blackout'),
+  navbar = document.querySelector('.navbar__list'),
+  menuItems = document.querySelectorAll('.navbar__item');
 
 burger.addEventListener('change', disableScrollingOnBody);
+burgerBlackout.addEventListener('click', closeMenu);
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => (burger.checked = false));
+});
 
 function disableScrollingOnBody() {
   if (burger.checked) {
@@ -9,5 +17,17 @@ function disableScrollingOnBody() {
   } else {
     document.body.classList.remove('disable-scroll');
   }
-  console.log(document.body.classList);
+}
+
+function closeMenu(evt) {
+  console.log(evt);
+  let targetEl = evt.target;
+  do {
+    if (targetEl == navbar) {
+      return;
+    }
+
+    targetEl = targetEl.parentNode;
+  } while (targetEl);
+  burger.checked = false;
 }
