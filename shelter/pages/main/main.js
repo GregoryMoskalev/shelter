0cae1a235e85d771cd5c525793ffc16c1958ab94
Gregory Moskalev <onberfrest@gmail.com>
2020-10-22 23:40:31 +0300

    feat: disable scroll with open menu

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -1,1 +1,13 @@
-console.log('ヾ(＾∇＾)')
+console.log('ヾ(＾∇＾)');
+const burger = document.querySelector('.navbar__checkbox');
+
+burger.addEventListener('change', disableScrollingOnBody);
+
+function disableScrollingOnBody() {
+  if (burger.checked) {
+    document.body.classList.add('disable-scroll');
+  } else {
+    document.body.classList.remove('disable-scroll');
+  }
+  console.log(document.body.classList);
+}
